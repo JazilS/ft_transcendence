ad3d0225a3a0fd0b10de6c0e68ff1ee86e7e45ab
Kenan GEZGIN <kgezgin@made-f0Br1s6.clusters.42paris.fr>
2024-01-24 13:45:54 +0100

    profile page in process

--- app/frontend/components/GameHistory.tsx
+++ app/frontend/components/GameHistory.tsx
@@ -19,8 +19,8 @@ const GameHistory: React.FC<GameHistoryProps> = ({ user }) =>{
 				.reverse()
 				.map((game) => (
 				<li key={game.id}>
-					<div className={`bg-white h-[100px] rounded-full text-black ${press_Start_2P} mb-10 flex flex-row p-5 items-center justify-between`}>
-						<div className={`flex flex-row items-center`}>
+					<div className={`bg-white h-[100px] rounded-full text-black mb-10 flex flex-row p-5 items-center justify-between`}>
+						<div className={`flex flex-row items-center ${press_Start_2P}`}>
 							<div className={`rounded-full mr-3 bg-white h-[70px] w-[70px] justify-center items-center overflow-hidden`}>
 								<Image src={user?.imageSrc} alt="profile picture" width={70} height={70}></Image>
 							</div>
