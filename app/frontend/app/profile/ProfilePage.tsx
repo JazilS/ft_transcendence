8c60818586fffcd49b90cc02aba93e87cd22bda1
Kenan GEZGIN <kgezgin@made-f0Br2s6.clusters.42paris.fr>
2024-02-19 10:14:09 +0100

    redux implementing

--- app/frontend/app/profile/ProfilePage.tsx
+++ app/frontend/app/profile/ProfilePage.tsx
@@ -6,8 +6,9 @@ import { UserProfile } from '@/models/ProfilePageModel'
 import { press_Start_2P, quantico } from '@/models/FontModel'
 import PlayerProfile from '@/components/molecules/PlayerProfile';
 import GameHistory from '../../components/molecules/GameHistory'
-import EditProfileButton from './EditProfile';
+import EditProfileButton from './EditAvatar';
 import '../styles.css'
+import EditUsername from '@/components/atom/EditUsername';
 
 export default function ProfilePage() {
 	const [user, setUser] = useState<UserProfile>({
@@ -17,7 +18,6 @@ export default function ProfilePage() {
 		games: [],
 	});
 	const [blur, setBlur] = useState(false)
-	const [isEditing, setIsEditing] = useState(true)
 
 	useEffect(() => {
 
@@ -35,14 +35,6 @@ export default function ProfilePage() {
 			]});
 	}, []);
 
-	const handleUsernameChange = (value: string) => {
-		setUser({ ...user, name: value });
-	};
-
-	const handleNameDisplay = (value: boolean) => {
-		setIsEditing(value);
-	};
-
 	let color: string
 	if (user?.isConnected === true )
 		color = "bg-green-500"
@@ -55,47 +47,7 @@ export default function ProfilePage() {
 					<div className={`h-[738px] w-[315px] flex flex-col justify-center items-center bg-gradient-to-br rounded-3xl from-indigo-500  to-fuchsia-500`}>
 						<EditProfileButton user={user} setUser={setUser} setBlur={setBlur}/>
 						<PlayerProfile user={user!} width={162} height={162} displayName={false} />
-						<div className='w-[315px] truncate mt-5'>
-							{isEditing ?
-								<div className={`h-[28px] w-[310] flex flex-row justify-center items-center space-y-5 truncate`}>
-									<div className={`w-[310] truncate text-center text-xl ${press_Start_2P.className}`}>
-										{user?.name}
-									</div>
-									<div className='pb-5 pl-2'>
-										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer" onClick={() => handleNameDisplay(false)}>
-											<path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
-										</svg>
-									</div>
-								</div>
-								 :
-							 	(<div className={`h-auto w-auto flex flex-row justify-center items-center space-y-5 `}>
-							 		<TextField
-										 inputProps={{
-											 style: {
-												 textAlign: "center",
-												 ...quantico.style
-											 }
-										 }}
-										 InputProps={{
-											 style: {
-												 borderRadius: "50px",
-												 background: 'rgba(255, 255, 255, 0.3)',
-												 border: 'none',
-											 }
-										 }}
-										 size='small'
-										 onChange={(event) => handleUsernameChange(event.target.value)}
-										 value={user ? user.name : "User"}
-							 		/>
-									<div className='pb-5 pl-2'>
-										<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 cursor-pointer"  onClick={() => handleNameDisplay(true)}>
-											<path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-										</svg>
-									</div>
-								</div>
-							 )}
-							{/* <PlayerProfile user={user!} width={162} height={162} IsEditing={IsEditing} /> */}
-						</div>
+						<EditUsername user={user} setUser={setUser}/>
 				<div className={`rounded-full h-3 w-3 ${color} blur-[2px]`} />
 				</div>
 					<div className='h-[738px] w-[855px] bg-gradient-to-br rounded-3xl from-fuchsia-500  to-indigo-500'>
